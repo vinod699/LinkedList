6121f05cb50efba11bdc2e236fb77fc4348bfd9d
vinod699 <kosarajuvinod9999@gmail.com>
2021-04-28 17:06:42 +0530

    uc2-Linked-List

--- src/com/linkedlist/MyNode.java
+++ src/com/linkedlist/MyNode.java
@@ -1,16 +1,24 @@
-package com.linkedlist;
-        import org.junit.jupiter.api.Assertions;
-        import org.junit.jupiter.api.Test;
 
-public class MyNode {
+package com.linkedlist;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyLinkedList {
+
     @Test
-    public void whenPassThreeNumber_LinkedListShouldPassedTest() {
-        MyNode<Integer> myFirstNode = new MyNode<>(56);
+    public void given3Numbers_WhenAddedToLinkedList_ShouldAddedToTop() {
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
         MyNode<Integer> mySecondNode = new MyNode<>(30);
-        MyNode<Integer> myThirdNode = new MyNode<>(70);
-        myFirstNode.setNext(mySecondNode);
-        mySecondNode.setNext(myThirdNode);
-        boolean result = myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode);
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.add(mySecondNode);
+        myLinkedList.add(myThirdNode);
+        myLinkedList.show();
+        boolean result = myLinkedList.head.equals(myThirdNode) &&
+                myLinkedList.head.getNext().equals(mySecondNode) &&
+                myLinkedList.tail.equals(myFirstNode);
         Assertions.assertTrue(result);
     }
 }
