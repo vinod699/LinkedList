f5a729afe61079a3489e01088887fc4af49b2522
vinod699 <kosarajuvinod9999@gmail.com>
2021-04-28 17:02:08 +0530

    uc1-Linked-List

--- src/com/linkedlist/MyNode.java
+++ src/com/linkedlist/MyNode.java
@@ -1,4 +1,16 @@
 package com.linkedlist;
+        import org.junit.jupiter.api.Assertions;
+        import org.junit.jupiter.api.Test;
 
 public class MyNode {
+    @Test
+    public void whenPassThreeNumber_LinkedListShouldPassedTest() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        myFirstNode.setNext(mySecondNode);
+        mySecondNode.setNext(myThirdNode);
+        boolean result = myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode);
+        Assertions.assertTrue(result);
+    }
 }
